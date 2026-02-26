8f5c6d6e6f10352c9a47bbfb2f6933418e833dd0
leonard trinh <trinhvandat90399@gmail.com>
2026-02-26 15:54:48 +0700

    add local kafka, mongo, add account for psql

--- docker/local/init-db/01-init-schemas.sql
+++ docker/local/init-db/01-init-schemas.sql
@@ -2,3 +2,15 @@ CREATE SCHEMA IF NOT EXISTS auth;
 CREATE USER auth_service WITH PASSWORD 'auth123';
 GRANT ALL PRIVILEGES ON SCHEMA auth TO auth_service;
 ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT ALL ON TABLES TO auth_service;
+
+-- Wallet Service Schema
+CREATE SCHEMA IF NOT EXISTS wallet;
+CREATE USER wallet_service WITH PASSWORD 'wallet123';
+GRANT ALL PRIVILEGES ON SCHEMA wallet TO wallet_service;
+ALTER DEFAULT PRIVILEGES IN SCHEMA wallet GRANT ALL ON TABLES TO wallet_service;
+
+-- Portfolio Service Schema
+CREATE SCHEMA IF NOT EXISTS portfolio;
+CREATE USER portfolio_service WITH PASSWORD 'portfolio123';
+GRANT ALL PRIVILEGES ON SCHEMA portfolio TO portfolio_service;
+ALTER DEFAULT PRIVILEGES IN SCHEMA portfolio GRANT ALL ON TABLES TO portfolio_service;

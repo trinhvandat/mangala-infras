54c1b78dacba2f7e70843c38c57d612f1a1b10ca
Leonard Trinh <trinhvandat90399@gmail.com>
2026-03-14 11:35:23 +0700

    update init db for crawler

--- docker/local/init-db/01-init-schemas.sql
+++ docker/local/init-db/01-init-schemas.sql
@@ -14,3 +14,19 @@ CREATE SCHEMA IF NOT EXISTS portfolio;
 CREATE USER portfolio_service WITH PASSWORD 'portfolio123';
 GRANT ALL PRIVILEGES ON SCHEMA portfolio TO portfolio_service;
 ALTER DEFAULT PRIVILEGES IN SCHEMA portfolio GRANT ALL ON TABLES TO portfolio_service;
+
+-- Crawler Service Schema
+CREATE SCHEMA IF NOT EXISTS crawler;
+
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'crawler_service') THEN
+    CREATE USER crawler_service WITH PASSWORD 'crawler123';
+  END IF;
+END
+$$;
+
+GRANT ALL PRIVILEGES ON SCHEMA crawler TO crawler_service;
+ALTER DEFAULT PRIVILEGES IN SCHEMA crawler GRANT ALL ON TABLES TO crawler_service;
+ALTER DEFAULT PRIVILEGES IN SCHEMA crawler GRANT ALL ON SEQUENCES TO crawler_service;
+ALTER DEFAULT PRIVILEGES IN SCHEMA crawler GRANT ALL ON FUNCTIONS TO crawler_service;
